9e79617e2e4ab54f05c99866b4a126fa46c84a0b
onford <1404533558@qq.com>
2024-07-08 05:25:12 -0700

    finish project1 without badger usage

--- kv/server/raw_api.go
+++ kv/server/raw_api.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"context"
+
+	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
@@ -11,26 +13,64 @@ import (
 // RawGet return the corresponding Get response based on RawGetRequest's CF and Key fields
 func (server *Server) RawGet(_ context.Context, req *kvrpcpb.RawGetRequest) (*kvrpcpb.RawGetResponse, error) {
 	// Your Code Here (1).
-	return nil, nil
+	reader, _ := server.storage.Reader(req.Context)
+	value, _ := reader.GetCF(req.Cf, req.Key)
+	notFound := value == nil
+	return &kvrpcpb.RawGetResponse{
+		Value:    value,
+		NotFound: notFound,
+	}, nil
 }
 
 // RawPut puts the target data into storage and returns the corresponding response
 func (server *Server) RawPut(_ context.Context, req *kvrpcpb.RawPutRequest) (*kvrpcpb.RawPutResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using Storage.Modify to store data to be modified
-	return nil, nil
+	_ = server.storage.Write(req.Context, []storage.Modify{
+		{
+			Data: storage.Put{
+				Cf:    req.Cf,
+				Key:   req.Key,
+				Value: req.Value,
+			},
+		},
+	})
+	// 暂时不知道要往 RawPutResponce 里面写什么，弄个空的
+	return &kvrpcpb.RawPutResponse{}, nil
 }
 
 // RawDelete delete the target data from storage and returns the corresponding response
 func (server *Server) RawDelete(_ context.Context, req *kvrpcpb.RawDeleteRequest) (*kvrpcpb.RawDeleteResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using Storage.Modify to store data to be deleted
-	return nil, nil
+	_ = server.storage.Write(req.Context, []storage.Modify{
+		{
+			Data: storage.Delete{
+				Cf:  req.Cf,
+				Key: req.Key,
+			},
+		},
+	})
+	return &kvrpcpb.RawDeleteResponse{}, nil
 }
 
 // RawScan scan the data starting from the start key up to limit. and return the corresponding result
 func (server *Server) RawScan(_ context.Context, req *kvrpcpb.RawScanRequest) (*kvrpcpb.RawScanResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using reader.IterCF
-	return nil, nil
+	reader, _ := server.storage.Reader(req.Context)
+	it := reader.IterCF(req.Cf)
+	var count uint32 = 0
+	var kvs []*kvrpcpb.KvPair
+	for it.Seek(req.StartKey); count < req.Limit && it.Valid(); count++ {
+		newPair := &kvrpcpb.KvPair{
+			Key: it.Item().Key(),
+		}
+		newPair.Value, _ = it.Item().Value()
+		kvs = append(kvs, newPair)
+		it.Next()
+	}
+	return &kvrpcpb.RawScanResponse{
+		Kvs: kvs,
+	}, nil
 }
--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -1,8 +1,12 @@
 package standalone_storage
 
 import (
+	// "fmt"
+	"sort"
+
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
@@ -10,11 +14,15 @@ import (
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
+	Conf config.Config
+	Data map[string]map[byte][]byte
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return nil
+	return &StandAloneStorage{
+		Conf: *conf,
+	}
 }
 
 func (s *StandAloneStorage) Start() error {
@@ -29,10 +37,126 @@ func (s *StandAloneStorage) Stop() error {
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
-	return nil, nil
+	return s, nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
 	// Your Code Here (1).
+	// 实现思路：StandAloneStorage 的 Data 映射 string -> map[byte][]byte
+	if s.Data == nil {
+		s.Data = make(map[string]map[byte][]byte)
+	}
+	for _, batchItem := range batch {
+		switch batchItem.Data.(type) {
+		case storage.Put:
+			keyValMap, cfExists := s.Data[batchItem.Cf()]
+			if !cfExists {
+				// 不存在对应 ColumnFamily 时
+				keyValMap = make(map[byte][]byte)
+				s.Data[batchItem.Cf()] = keyValMap
+			}
+			for _, key := range batchItem.Key() {
+				keyValMap[key] = append(keyValMap[key], batchItem.Value()...)
+			}
+		case storage.Delete:
+			keyValMap, cfExists := s.Data[batchItem.Cf()]
+			if cfExists {
+				// 只有存在对应的 ColumnFamily 才可以删除
+				for _, key := range batchItem.Key() {
+					delete(keyValMap, key)
+				}
+			}
+		}
+	}
 	return nil
+}
+
+/*自己增加三个函数，使得 StandAloneStorage 实现接口 storage.StorageReader*/
+
+func (s *StandAloneStorage) GetCF(cf string, key []byte) ([]byte, error) {
+	keyValMap, cfExists := s.Data[cf]
+	var value []byte
+	if cfExists {
+		for _, kkey := range key {
+			if vvalue, ok := keyValMap[kkey]; ok {
+				value = append(value, vvalue...)
+			}
+			// 这里直接忽略了部分 key 不存在,但是部分 key 存在的情况
+			// 所有 key 均不存在，返回 nil
+		}
+		return value, nil
+	}
+	return nil, nil
+}
+
+func (s *StandAloneStorage) IterCF(cf string) engine_util.DBIterator {
+	// 这个还没实现
+	var keys []byte
+	mmap := make(map[byte][]byte)
+	for key, value := range s.Data[cf] {
+		mmap[key] = value
+		keys = append(keys, key)
+	}
+	sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
+	return &StandAloneIterator{
+		Data:  mmap,
+		Key:   keys,
+		index: 0,
+	}
+}
+
+func (s *StandAloneStorage) Close() {}
+
+/*构造结构体 StandAloneIterator 实现接口 DBIterator,是当前数据库的快照*/
+
+type StandAloneIterator struct {
+	index int
+	Key   []byte
+	Data  map[byte][]byte
+}
+
+func (s *StandAloneIterator) Item() engine_util.DBItem {
+	if !s.Valid() {
+		return nil
+	}
+	vvalue := make([]byte, len(s.Data[s.Key[s.index]]))
+	copy(vvalue, s.Data[s.Key[s.index]])
+	// fmt.Println("Vvalue:%s", vvalue, s.Data[s.Key[s.index]])
+	return &StandAloneItem{
+		Kkey:   []byte{s.Key[s.index]},
+		Vvalue: vvalue,
+	}
+}
+
+func (s *StandAloneIterator) Valid() bool { return s.index < len(s.Key) }
+func (s *StandAloneIterator) Next()       { s.index++ }
+func (s *StandAloneIterator) Seek(val []byte) {
+	s.index = sort.Search(len(s.Key), func(i int) bool { return s.Key[i] >= val[0] })
+	// 如果不存在，index 指向最末尾
+}
+func (s *StandAloneIterator) Close() {}
+
+/*构造结构体 StandAloneItem 实现接口 DBItem*/
+
+type StandAloneItem struct {
+	Kkey   []byte
+	Vvalue []byte
+}
+
+func (s *StandAloneItem) Key() []byte { return s.Kkey }
+func (s *StandAloneItem) KeyCopy(dst []byte) []byte {
+	if len(dst) < len(s.Kkey) {
+		dst = make([]byte, len(s.Kkey))
+	}
+	copy(dst, s.Kkey)
+	return dst
+}
+func (s *StandAloneItem) Value() ([]byte, error) { return s.Vvalue, nil }
+func (s *StandAloneItem) ValueSize() int         { return len(s.Vvalue) }
+func (s *StandAloneItem) ValueCopy(dst []byte) ([]byte, error) {
+	if len(dst) < len(s.Vvalue) {
+		dst = make([]byte, len(s.Vvalue))
+	}
+	copy(dst, s.Vvalue)
+	return dst, nil
 }
